120b2ffa340be7fd52d3e4023244edf76e1ee1cf
learn_more <learn_more@unknowncheats.me>
2013-08-30 23:58:26 +0200

    FindGLEW2 uses default naming

--- cmake/FindGLEW2.cmake
+++ cmake/FindGLEW2.cmake
@@ -3,12 +3,12 @@
 # Once done this will define
 #
 # GLEW_FOUND
-# GLEW_INCLUDE_PATH
+# GLEW_INCLUDE_DIR
 # GLEW_LIBRARY
 # 
 
 IF (WIN32)
-	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+	FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
 		$ENV{PROGRAMFILES}/GLEW/include
 		${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
 		DOC "The directory where GL/glew.h resides")
@@ -20,7 +20,7 @@ IF (WIN32)
 		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
 		DOC "The GLEW library")
 ELSE (WIN32)
-	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+	FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
 		/usr/include
 		/usr/local/include
 		/sw/include
@@ -38,10 +38,10 @@ ELSE (WIN32)
 		DOC "The GLEW library")
 ENDIF (WIN32)
 
-IF (GLEW_INCLUDE_PATH)
+IF (GLEW_INCLUDE_DIR)
 	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ELSE (GLEW_INCLUDE_PATH)
+ELSE (GLEW_INCLUDE_DIR)
 	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ENDIF (GLEW_INCLUDE_PATH)
+ENDIF (GLEW_INCLUDE_DIR)
 
 MARK_AS_ADVANCED( GLEW_FOUND )
