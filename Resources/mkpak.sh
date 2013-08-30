176944f3bb966572d26ff9dd1a72d5436907c42a
yvt <i@yvt.jp>
2013-08-31 04:53:28 +0900

    * added PakLocation.txt to indicate URL for the latest oaks (I cannot add contents of each .pak because of license problems, and adding .pak to a git repository is not good) * modified mkpak.sh so that it can package all paks into one zip file

--- Resources/mkpak.sh
+++ Resources/mkpak.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-rm -f Base.pak Sounds.pak Models.pak
+rm -f Base.pak Sounds.pak Models.pak DevPaks.pak
 
 zip -r Base.pak Gfx Textures
 zip -r Models.pak Models
 zip -r Sounds.pak Sounds
 
+zip DevPaks.zip Base.pak Models.pak Sounds.pak
