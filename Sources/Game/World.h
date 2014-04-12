5f46d80924f349ce02702a2b7f82bd6a229508e6
yvt <i@yvt.jp>
2014-04-13 01:52:31 +0900

    Added Server.cpp/World.cpp for next-gen protocol (no class definition yet)

---
+++ Sources/Game/World.h
@@ -0,0 +1,16 @@
+//
+//  World.h
+//  OpenSpades
+//
+//  Created by Tomoaki Kawada on 4/13/14.
+//  Copyright (c) 2014 yvt.jp. All rights reserved.
+//
+
+#pragma once
+
+namespace spades { namespace game {
+	
+	// nothing here yet
+	
+} }
+
