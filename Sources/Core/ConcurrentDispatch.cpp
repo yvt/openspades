fd7c0a874c218d83de1c507f5920eb62d02a3665
Dmitry Marakasov <amdmi3@amdmi3.ru>
2014-07-31 00:40:27 +0400

    sys/sysinfo.h is specific ot Linux, not gcc

--- Sources/Core/ConcurrentDispatch.cpp
+++ Sources/Core/ConcurrentDispatch.cpp
@@ -38,7 +38,7 @@
 #ifndef _MSC_VER
 #include <unistd.h>
 #endif
-#if defined(__GNUC__)
+#if defined(__linux__)
 #include <sys/sysinfo.h>
 #endif
 #endif
@@ -67,7 +67,7 @@ static int GetNumCores() {
         if(count < 1) { count = 1; }
     }
     return count;
-#elif defined(__GNUC__)
+#elif defined(__linux__)
     return get_nprocs();
 #else
     return sysconf(_SC_NPROCESSORS_ONLN);
