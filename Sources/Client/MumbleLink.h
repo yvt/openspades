03373a68be3fb74c93a4983458ffdb5c435890b7
L0L022 <L0L022@openmailbox.org>
2016-08-05 13:53:16 +0200

    Use namespace spades

--- Sources/Client/MumbleLink.h
+++ Sources/Client/MumbleLink.h
@@ -5,39 +5,41 @@
 #include <windef.h>
 #endif
 
-struct MumbleLinkedMemory {
-  uint32_t uiVersion;
-  uint32_t uiTick;
-  float fAvatarPosition[3];
-  float fAvatarFront[3];
-  float fAvatarTop[3];
-  wchar_t name[256];
-  float fCameraPosition[3];
-  float fCameraFront[3];
-  float fCameraTop[3];
-  wchar_t identity[256];
-  uint32_t context_len;
-  unsigned char context[256];
-  wchar_t description[2048];
-};
+namespace spades {
+    struct MumbleLinkedMemory {
+      uint32_t uiVersion;
+      uint32_t uiTick;
+      float fAvatarPosition[3];
+      float fAvatarFront[3];
+      float fAvatarTop[3];
+      wchar_t name[256];
+      float fCameraPosition[3];
+      float fCameraFront[3];
+      float fCameraTop[3];
+      wchar_t identity[256];
+      uint32_t context_len;
+      unsigned char context[256];
+      wchar_t description[2048];
+    };
 
-class MumbleLink {
-  const float metre_per_block;
-  MumbleLinkedMemory *mumbleLinkedMemory;
-#ifdef _WIN32
-  HANDLE obj;
-#else
-  int fd;
-#endif
+    class MumbleLink {
+      const float metre_per_block;
+      MumbleLinkedMemory *mumbleLinkedMemory;
+    #ifdef _WIN32
+      HANDLE obj;
+    #else
+      int fd;
+    #endif
 
-  void set_mumble_vector3(float mumble_vec[3], const spades::Vector3 &vec);
+      void set_mumble_vector3(float mumble_vec[3], const spades::Vector3 &vec);
 
-public:
-  MumbleLink();
-  ~MumbleLink();
+    public:
+      MumbleLink();
+      ~MumbleLink();
 
-  bool init();
-  void setContext(const std::string &context);
-  void setIdentity(const std::string &identity);
-  void update(spades::client::Player *player);
-};
+      bool init();
+      void setContext(const std::string &context);
+      void setIdentity(const std::string &identity);
+      void update(spades::client::Player *player);
+    };
+}
