245cd210a346419f28dfeb4ea80844e6004e755b
VelocityRa <makren67@gmail.com>
2017-01-05 02:13:04 +0200

    Fix VersionInfo errors, add Windows Server checking

--- Sources/Core/VersionInfo.cpp
+++ Sources/Core/VersionInfo.cpp
@@ -17,37 +17,39 @@ std::string VersionInfo::GetVersionInfo() {
 #elif defined(TARGET_OS_MAC)
 	return std::string("Mac OS X");
 #elif defined(OS_PLATFORM_WINDOWS)
-		// TODO: IsWindowsServer()
-
-		if((IsWindowsXPOrGreater()) && !(IsWindowsVistaOrGreater())) {
-			return std::string("Windows XP");
-		}
-		
-		if((IsWindowsVistaOrGreater()) && !(IsWindows7OrGreater())) {
-			return std::string("Windows Vista");
-		}
-		
-		if((IsWindows7OrGreater()) && !(IsWindows8OrGreater())) {
-			return std::string("Windows 7");
-		}
-		
-		if((IsWindows8OrGreater()) && !(IsWindows8Point1OrGreater())) {
-			return std::string("Windows 8");
-		}
-		
-		if((IsWindows8Point1OrGreater()) && !(IsWindows10OrGreater())) {
-			return std::string("Windows 8.1");
-		}
-		
-		if((IsWindows10OrGreater())) {
-			return std::string("Windows 10");
-			
-			//   Might be a greater version, but the new Microsoft
-			// API doesn't support checking for specific versions.            
-		}
-	}
 	
-	return std::string("Windows ??");
+	std::string windowsVersion;
+
+	if (IsWindowsXPOrGreater() && !IsWindowsVistaOrGreater()) {
+		windowsVersion = "Windows XP";
+	}
+
+	if (IsWindowsVistaOrGreater() && !IsWindows7OrGreater()) {
+		windowsVersion = "Windows Vista";
+	}
+
+	if (IsWindows7OrGreater() && !IsWindows8OrGreater()) {
+		windowsVersion = "Windows 7";
+	}
+
+	if (IsWindows8OrGreater() && !IsWindows8Point1OrGreater()) {
+		windowsVersion = "Windows 8";
+	}
+
+	if (IsWindows8Point1OrGreater()) {
+		windowsVersion = "Windows 8.1";
+	}
+
+	// Might be a greater version, but the new Microsoft
+	// API doesn't support checking for specific versions.
+
+	// Default to Windows 10
+	// See https://github.com/yvt/openspades/pull/528 for reason.
+	windowsVersion = "Windows 10";
+
+	if (IsWindowsServer())
+		windowsVersion += " Server";
+	return windowsVersion;
 #else
 	return std::string("Unknown OS");
 #endif
