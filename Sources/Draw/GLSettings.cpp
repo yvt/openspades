61a2647697487bd96f603b69f2f09b1ff29100b7
yvt <i@yvt.jp>
2017-02-22 08:19:22 +0900

    Add r_saturation

--- Sources/Draw/GLSettings.cpp
+++ Sources/Draw/GLSettings.cpp
@@ -56,6 +56,7 @@ DEFINE_SPADES_SETTING(r_occlusionQuery, "0");
 DEFINE_SPADES_SETTING(r_optimizedVoxelModel, "1");
 DEFINE_SPADES_SETTING(r_physicalLighting, "0");
 DEFINE_SPADES_SETTING(r_radiosity, "0");
+DEFINE_SPADES_SETTING(r_saturation, "1");
 DEFINE_SPADES_SETTING(r_shadowMapSize, "2048");
 DEFINE_SPADES_SETTING(r_softParticles, "1");
 DEFINE_SPADES_SETTING(r_sparseShadowMaps, "1");
