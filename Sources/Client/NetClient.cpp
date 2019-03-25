a662ebe0d0588b709ffee30ff921093505b5b3d5
NotAFile <notafile@gmail.com>
2019-03-25 15:12:38 +0100

    fix case value

--- Sources/Client/NetClient.cpp
+++ Sources/Client/NetClient.cpp
@@ -755,7 +755,7 @@ namespace spades {
 				case 2: return _Tr("NetClient", "You have too many connections to this server.");
 				case 3: return _Tr("NetClient", "Incompatible client protocol version.");
 				case 4: return _Tr("NetClient", "Server full");
-				case 4: return _Tr("NetClient", "Server was shut down");
+				case 5: return _Tr("NetClient", "Server was shut down");
 				case 10: return _Tr("NetClient", "You were kicked from this server.");
 				case 20: return _Tr("NetClient", "Invalid name");
 				default: return _Tr("NetClient", "Unknown Reason");
