b9426fd43815e27cb1efb6b7266489e0003df62c
yvt <i@yvt.jp>
2019-05-19 00:55:24 +0900

    Add `RandomAccessAdapter`
    It will be used to aid the implementation of the improved map loader.

---
+++ Sources/Core/RandomAccessAdaptor.cpp
@@ -0,0 +1,75 @@
+/*
+ Copyright (c) 2019 yvt
+
+ This file is part of OpenSpades.
+
+ OpenSpades is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenSpades is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenSpades.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include "RandomAccessAdaptor.h"
+
+#include <Core/Exception.h>
+#include <Core/IStream.h>
+#include <string>
+
+namespace spades {
+	RandomAccessAdaptor::RandomAccessAdaptor(IStream &inner) : inner{inner} {}
+
+	RandomAccessAdaptor::~RandomAccessAdaptor() {
+		// Do nothing - the buffer is automatically taken care of
+	}
+
+	void RandomAccessAdaptor::ExpandTo(std::size_t newBufferSize) {
+		if (newBufferSize <= buffer.size()) {
+			return;
+		}
+
+		std::size_t numAdditionalBytes = newBufferSize - buffer.size();
+		std::size_t originalSize = buffer.size();
+
+		try {
+			// Expand the internal buffer
+			buffer.resize(newBufferSize);
+
+			// Read additional bytes from the underlying stream to the newly
+			// allocated space of the internal buffer
+			std::size_t numBytesRead = inner.Read(buffer.data() + originalSize, numAdditionalBytes);
+
+			// Shrink the buffer to the actual read size
+			buffer.resize(originalSize + numBytesRead);
+		} catch (...) {
+			// Roll back to the original state
+			buffer.resize(originalSize);
+			throw;
+		}
+	}
+
+	bool RandomAccessAdaptor::TryRead(std::size_t offset, std::size_t size, char *output) {
+		std::size_t newLen = offset + size;
+		if (newLen < offset) {
+			// `newLen` overflowed `size_t`.
+			SPRaise("integer overflow");
+		}
+
+		ExpandTo(newLen);
+
+		if (buffer.size() < newLen) {
+			// Reached EOF
+			return false;
+		}
+
+		std::memcpy(output, buffer.data() + offset, size);
+		return true;
+	}
+} // namespace spades
---
+++ Sources/Core/RandomAccessAdaptor.h
@@ -0,0 +1,86 @@
+/*
+ Copyright (c) 2019 yvt
+
+ This file is part of OpenSpades.
+
+ OpenSpades is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenSpades is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenSpades.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include <vector>
+
+#include <Core/TMPUtils.h>
+
+namespace spades {
+	class IStream;
+
+	/**
+	 * Wraps a read-only stream to provide a random-accessible view into the data using a
+	 * dynamically-growing internal buffer.
+	 */
+	class RandomAccessAdaptor {
+	public:
+		/**
+		 * Constructs a `RandomAccessAdaptor` using the specified stream to supply the data read
+		 * through the view.
+		 *
+		 * The specified stream must outlive the uses of the constructed `RandomAccessAdaptor`.
+		 */
+		RandomAccessAdaptor(IStream &inner);
+
+		/**
+		 * De-initializes a `RandomAcesssAdaptor` and release all resources associates with it.
+		 *
+		 * Note that the wrapped stream is not closed by this finalizer.
+		 */
+		~RandomAccessAdaptor();
+
+		/**
+		 * Try to read the value of type `T` at the specified offset.
+		 *
+		 * Various complicated rules regarding reinterpretation apply, so it's not generally
+		 * a good idea to specfiy a non-POD type as `T`.
+		 *
+		 * `offset + sizeof(T)` must not overflow `size_t`. An exception is thrown in such a case,
+		 * but probably should abort the program in the future.
+		 */
+		template <class T> stmp::optional<T> TryRead(std::size_t offset) {
+			T data;
+			if (TryRead(offset, sizeof(T), reinterpret_cast<char *>(&data))) {
+				return {data};
+			} else {
+				return {};
+			}
+		}
+
+	private:
+		/**
+		 * Tries to ensure `buffer` is at least `newBufferSize` bytes long.
+		 *
+		 * The final size might be less than `newBufferSize` if an EOF is reached. This function is
+		 * exception-safe - `buffer` is left in the original state if an exception occurs while
+		 * reading the inner stream.
+		 */
+		void ExpandTo(std::size_t newBufferSize);
+
+		/**
+		 * Try to read `size` bytes at the specified offset to `output`.
+		 *
+		 * @return `true` if all of the bytes could be read.
+		 */
+		bool TryRead(std::size_t offset, std::size_t size, char *output);
+
+		IStream &inner;
+		std::vector<char> buffer;
+	};
+} // namespace spades
