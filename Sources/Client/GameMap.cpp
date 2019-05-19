f19469f6fe904cd7bd7b3e9002f8a21e3d766946
yvt <i@yvt.jp>
2019-05-19 13:03:35 +0900

    clang-format

--- Sources/Client/GameMap.cpp
+++ Sources/Client/GameMap.cpp
@@ -525,71 +525,71 @@ namespace spades {
 
 			Handle<GameMap> map{new GameMap(), false};
 
-				for (int y = 0; y < 512; y++) {
-					for (int x = 0; x < 512; x++) {
-						map->solidMap[x][y] = 0xffffffffffffffffULL;
+			for (int y = 0; y < 512; y++) {
+				for (int x = 0; x < 512; x++) {
+					map->solidMap[x][y] = 0xffffffffffffffffULL;
 
-						if (pos + 2 >= len) {
+					if (pos + 2 >= len) {
+						SPRaise("File truncated");
+					}
+
+					int z = 0;
+					for (;;) {
+						int i;
+						uint32_t *color;
+						int number_4byte_chunks = bytes[pos];
+						int top_color_start = bytes[pos + 1];
+						int top_color_end = bytes[pos + 2];
+						int bottom_color_start;
+						int bottom_color_end;
+						int len_top;
+						int len_bottom;
+
+						for (i = z; i < top_color_start; i++)
+							map->Set(x, y, i, false, 0, true);
+
+						if (pos + 4 + top_color_end - top_color_start + 3 >= len) {
 							SPRaise("File truncated");
 						}
 
-						int z = 0;
-						for (;;) {
-							int i;
-							uint32_t *color;
-							int number_4byte_chunks = bytes[pos];
-							int top_color_start = bytes[pos + 1];
-							int top_color_end = bytes[pos + 2];
-							int bottom_color_start;
-							int bottom_color_end;
-							int len_top;
-							int len_bottom;
+						color = (uint32_t *)(bytes.data() + pos + 4);
+						for (z = top_color_start; z <= top_color_end; z++)
+							map->Set(x, y, z, true, swapColor(*(color++)), true);
 
-							for (i = z; i < top_color_start; i++)
-								map->Set(x, y, i, false, 0, true);
+						if (top_color_end == 62) {
+							map->Set(x, y, 63, true, map->GetColor(x, y, 62), true);
+						}
 
-							if (pos + 4 + top_color_end - top_color_start + 3 >= len) {
-								SPRaise("File truncated");
-							}
+						len_bottom = top_color_end - top_color_start + 1;
 
-							color = (uint32_t *)(bytes.data() + pos + 4);
-							for (z = top_color_start; z <= top_color_end; z++)
-								map->Set(x, y, z, true, swapColor(*(color++)), true);
+						if (number_4byte_chunks == 0) {
+							pos += 4 * (len_bottom + 1);
+							break;
+						}
 
-							if (top_color_end == 62) {
-								map->Set(x, y, 63, true, map->GetColor(x, y, 62), true);
-							}
+						len_top = (number_4byte_chunks - 1) - len_bottom;
 
-							len_bottom = top_color_end - top_color_start + 1;
+						pos += (int)bytes[pos] * 4;
 
-							if (number_4byte_chunks == 0) {
-								pos += 4 * (len_bottom + 1);
-								break;
-							}
+						if (pos + 3 >= len) {
+							SPRaise("File truncated");
+						}
 
-							len_top = (number_4byte_chunks - 1) - len_bottom;
+						bottom_color_end = bytes[pos + 3];
+						bottom_color_start = bottom_color_end - len_top;
 
-							pos += (int)bytes[pos] * 4;
-
-							if (pos + 3 >= len) {
-								SPRaise("File truncated");
-							}
-
-							bottom_color_end = bytes[pos + 3];
-							bottom_color_start = bottom_color_end - len_top;
-
-							for (z = bottom_color_start; z < bottom_color_end; z++) {
-								uint32_t col = swapColor(*(color++));
-								map->Set(x, y, z, true, col, true);
-							}
-							if (bottom_color_end == 63) {
-								map->Set(x, y, 63, true, map->GetColor(x, y, 62), true);
-							}
+						for (z = bottom_color_start; z < bottom_color_end; z++) {
+							uint32_t col = swapColor(*(color++));
+							map->Set(x, y, z, true, col, true);
+						}
+						if (bottom_color_end == 63) {
+							map->Set(x, y, 63, true, map->GetColor(x, y, 62), true);
 						}
 					}
 				}
+			}
 
 			return map.Unmanage();
 		}
-	}
-}
+	} // namespace client
+} // namespace spades
