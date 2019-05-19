1a2287201d07900c573a192c06330e91e97edb09
yvt <i@yvt.jp>
2019-05-19 15:38:35 +0900

    Add `GameMapLoader`

---
+++ Sources/Client/GameMapLoader.cpp
@@ -0,0 +1,150 @@
+/*
+ Copyright (c) 2019 yvt
+
+ This file is part of OpenSpades.
+
+ OpenSpades is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenSpades is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenSpades.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include <exception>
+
+#include "GameMap.h"
+#include "GameMapLoader.h"
+#include <Core/Debug.h>
+#include <Core/DeflateStream.h>
+#include <Core/Exception.h>
+#include <Core/IRunnable.h>
+#include <Core/PipeStream.h>
+#include <Core/Thread.h>
+
+namespace spades {
+	namespace client {
+
+		struct GameMapLoader::Result {
+			// The following fields are mutually exclusive.
+			std::exception_ptr exceptionThrown;
+			Handle<GameMap> gameMap;
+		};
+
+		struct GameMapLoader::Decoder : public IRunnable {
+			GameMapLoader &parent;
+			StreamHandle rawDataReader;
+
+			Decoder(GameMapLoader &parent, StreamHandle rawDataReader)
+			    : parent{parent}, rawDataReader{rawDataReader} {}
+
+			void Run() override {
+				SPADES_MARK_FUNCTION();
+
+				std::unique_ptr<Result> result{new Result()};
+
+				try {
+					DeflateStream inflate(&*rawDataReader, CompressModeDecompress, false);
+
+					GameMap *gameMapPtr =
+					  GameMap::Load(&inflate, [this](int x) { HandleProgress(x); });
+
+					result->gameMap = Handle<GameMap>{gameMapPtr, false};
+				} catch (...) {
+					// Capture the current exception
+					result->exceptionThrown = std::current_exception();
+				}
+
+				// Send back the result
+				parent.resultCell.store(std::move(result));
+			}
+
+			void HandleProgress(int numColumnsLoaded) {
+				parent.progressCell.store(numColumnsLoaded);
+			}
+		};
+
+		GameMapLoader::GameMapLoader() {
+			SPADES_MARK_FUNCTION();
+
+			auto pipe = CreatePipeStream();
+
+			rawDataWriter = StreamHandle{std::get<0>(pipe)};
+			auto rawDataReader = StreamHandle{std::get<1>(pipe)};
+
+			decodingThreadRunnable.reset(new Decoder(*this, rawDataReader));
+
+			// Drop `rawDataReader` before the thread starts. `StreamHandle`'s internally
+			// ref-counted and it's not thread-safe. So, if we don't drop it here, there'll be
+			// a data race between the constructor of `Decoder` and the deconstruction of the local
+			// variable `rawDataReader`.
+			rawDataReader = StreamHandle{};
+
+			decodingThread.reset(new Thread(&*decodingThreadRunnable));
+		}
+
+		GameMapLoader::~GameMapLoader() {
+			SPADES_MARK_FUNCTION();
+
+			// Hang up the writer. This causes the decoder thread to exit gracefully.
+			rawDataWriter = StreamHandle{};
+
+			decodingThread->Join();
+			decodingThread.reset();
+		}
+
+		void GameMapLoader::AddRawChunk(const char *bytes, std::size_t numBytes) {
+			SPADES_MARK_FUNCTION();
+
+			if (!rawDataWriter) {
+				SPRaise("The raw data channel is already closed.");
+			}
+
+			rawDataWriter->Write(bytes, numBytes);
+		}
+
+		void GameMapLoader::MarkEOF() {
+			SPADES_MARK_FUNCTION();
+
+			if (!rawDataWriter) {
+				SPRaise("The raw data channel is already closed.");
+			}
+			rawDataWriter = StreamHandle{};
+		}
+
+		bool GameMapLoader::IsComplete() { return resultCell.operator bool(); }
+
+		void GameMapLoader::WaitComplete() {
+			SPADES_MARK_FUNCTION();
+
+			decodingThread->Join();
+
+			SPAssert(IsComplete());
+		}
+
+		float GameMapLoader::GetProgress() {
+			return static_cast<float>(progressCell.load(std::memory_order_relaxed)) / (512 * 512);
+		}
+
+		GameMap *GameMapLoader::TakeGameMap() {
+			SPADES_MARK_FUNCTION();
+
+			SPAssert(IsComplete());
+
+			std::unique_ptr<Result> result = resultCell.take();
+
+			if (result->gameMap) {
+				return result->gameMap.Unmanage();
+			} else {
+				std::rethrow_exception(result->exceptionThrown);
+			}
+		}
+
+	} // namespace client
+} // namespace spades
---
+++ Sources/Client/GameMapLoader.h
@@ -0,0 +1,105 @@
+/*
+ Copyright (c) 2019 yvt
+
+ This file is part of OpenSpades.
+
+ OpenSpades is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenSpades is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenSpades.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+#include <atomic>
+#include <memory>
+
+#include <Core/IStream.h>
+#include <Core/TMPUtils.h>
+
+namespace spades {
+	class Thread;
+	class IRunnable;
+
+	namespace client {
+
+		class GameMap;
+
+		/**
+		 * A streaming map loader that can decode a game map in a streaming fashion and
+		 * report the progress based on the incomplete decoded data.
+		 */
+		class GameMapLoader {
+		public:
+			GameMapLoader();
+			~GameMapLoader();
+
+			GameMapLoader(const GameMapLoader &) = delete;
+			void operator=(const GameMapLoader &) = delete;
+
+			/**
+			 * Adds undecoded data to the internal buffer for decoding.
+			 */
+			void AddRawChunk(const char *bytes, std::size_t numBytes);
+
+			/**
+			 * Notifies the decoder that there is no more undecoded data to process.
+			 */
+			void MarkEOF();
+
+			/**
+			 * Returns `true` if the loading operation is complete, successful or not.
+			 */
+			bool IsComplete();
+
+			/**
+			 * Blocks the current thread until the decoding is complete.
+			 * Make sure to call `MakeEOF` first or it might cause a deadlock.
+			 */
+			void WaitComplete();
+
+			/**
+			 * Gets how much portion of the map has completed loading.
+			 *
+			 * @return A value in range `[0, 1]`.
+			 */
+			float GetProgress();
+
+			/**
+			 * Gets the loaded `GameMap` and takes the ownership of it.
+			 *
+			 * `IsComplete()` must be `true`.
+			 *
+			 * If an exception occured while decoding the map, the exception will be rethrown when
+			 * this method is called.
+			 */
+			GameMap *TakeGameMap();
+
+		private:
+			struct Decoder;
+			struct Result;
+
+			/** A writable stream used to send undecoded data to the decoding thread. */
+			StreamHandle rawDataWriter;
+
+			/** A handle for the decoding thread. */
+			std::unique_ptr<Thread> decodingThread;
+
+			/** The `IRunnable` used to create `decodingThread`. Must outlive the thread. */
+			std::unique_ptr<IRunnable> decodingThreadRunnable;
+
+			/** The cell for receiving the decode progress. */
+			std::atomic_uint32_t progressCell;
+
+			/** The cell for receiving the decode result. */
+			stmp::atomic_unique_ptr<Result> resultCell;
+		};
+
+	} // namespace client
+} // namespace spades
