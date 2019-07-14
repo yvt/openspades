f4f9dce529dfe5d9dc0bf63430af649a512b77f2
yvt <i@yvt.jp>
2019-07-14 17:26:19 +0900

    Add `#pragma once`

--- Sources/Gui/ConsoleHelper.h
+++ Sources/Gui/ConsoleHelper.h
@@ -16,6 +16,8 @@
  You should have received a copy of the GNU General Public License
  along with OpenSpades.  If not, see <http://www.gnu.org/licenses/>.
  */
+#pragma once
+
 #include <Core/RefCountedObject.h>
 
 namespace spades {
