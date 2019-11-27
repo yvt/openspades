17cd56dd694a96f30ed9342c894461f66e8bb578
dtomlinson-ga <dtomlinson-ga@users.noreply.github.com>
2019-11-27 00:27:55 -0500

    Fix use after free in Main.cpp (issue #846)
    Create c-string to store filepath before concatenating with output message.

--- Sources/Gui/Main.cpp
+++ Sources/Gui/Main.cpp
@@ -425,9 +425,8 @@ int main(int argc, char **argv) {
 						SDL_RWops *io = SDL_RWFromFile(
 						  (home + "/.openspades/CONTENT_MOVED_TO_NEW_DIR").c_str(), "wb");
 						if (io != NULL) {
-							const char *text = ("Content of this directory moved to " +
-							                    xdg_data_home + "/openspades")
-							                     .c_str();
+							const char *directory = (xdg_data_home + "/openspades").c_str();
+							const char *text = ("Content of this directory moved to " + directory).c_str();
 							io->write(io, text, strlen(text), 1);
 							io->close(io);
 						}
