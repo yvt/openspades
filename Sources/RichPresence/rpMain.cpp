215c62edb3e0665d350eda3d9e2781ee8c718037
SrDosPastel <srdospastel807@gmail.com>
2020-10-08 14:34:14 -0300

    Removed the localhost verify

--- Sources/RichPresence/rpMain.cpp
+++ Sources/RichPresence/rpMain.cpp
@@ -40,7 +40,6 @@ int rpM(){
 }
 
 int atualizarRP(){
-  SPADES_SETTING(cg_lastQuickConnectHost);
   SPADES_SETTING(cg_playerName);
 
   interrupted = true;
@@ -48,31 +47,20 @@ int atualizarRP(){
 
   std::string player = cg_playerName.operator std::string().substr(0, 15).c_str();
   std::string playerStr = "Playing as: " + player;
-  std::string checkIp = cg_lastQuickConnectHost.operator std::string().substr(0, 15).c_str();
 
-  // If the user isnt playing on localhost the client will send the normal infos about the server
-  if(checkIp.rfind("aos://16777343", 0) != 0 || checkIp.rfind("localhost", 0) != 0 || checkIp.rfind("127.0.0.1", 0) != 0){
-    Json::Value infos = serverInfoM();
+  Json::Value infos = serverInfoM();
 
-    std::string map = "Map: "+ infos["map"].asString();
-    std::string online = "Players: "+std::to_string(infos["players_current"].asInt())
-    + "/"+std::to_string(infos["players_max"].asInt());
+  std::string map = "Map: "+ infos["map"].asString();
+  std::string online = "Players: "+std::to_string(infos["players_current"].asInt())
+  + "/"+std::to_string(infos["players_max"].asInt());
 
-    activity.SetDetails(infos["name"].asCString());
-    activity.SetState(&online[0]);
-    activity.GetAssets().SetLargeImage("openspadesl");
-    activity.GetAssets().SetSmallImage("openspadesf");
-    activity.GetAssets().SetSmallText(&map[0]);
-    activity.GetAssets().SetLargeText(&playerStr[0]);
-    activity.SetType(discord::ActivityType::Playing);
-  } else {
-    activity.SetDetails("Playing in Localhost!");
-    activity.SetState(&playerStr[0]);
-    activity.GetAssets().SetLargeImage("openspadesl");
-    activity.GetAssets().SetSmallImage("openspadesf");
-    activity.GetAssets().SetLargeText("Shooting on Blocks");
-    activity.SetType(discord::ActivityType::Playing);
-  }
+  activity.SetDetails(infos["name"].asCString());
+  activity.SetState(&online[0]);
+  activity.GetAssets().SetLargeImage("openspadesl");
+  activity.GetAssets().SetSmallImage("openspadesf");
+  activity.GetAssets().SetSmallText(&map[0]);
+  activity.GetAssets().SetLargeText(&playerStr[0]);
+  activity.SetType(discord::ActivityType::Playing);
 
   core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {
     std::cout << ((result == discord::Result::Ok) ? "Succeeded" : "Failed")
