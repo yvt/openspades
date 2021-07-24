ee1f8a4772a17206a6628b8613553c8c7c9bb966
yvt <i@yvt.jp>
2021-07-24 18:48:18 +0900

    fix(client): relax the lower bound of the denominator used in numerical differentiation for velocity estimation
    Improves the smoothness of non-local player movement in a server that
    sends ten WorldUpdate packages for each second (i.e., 10 updates per
    second).
    
    I wouldn't dare to relax the bound further since this technique is
    extremely susceptible to jitter. The server sending WorldUpdate packets
    with `PACKET_FLAG_UNSEQUENCED` isn't exactly helping since that
    introduces discontinuity into the received position stream, and the
    denominator lower bound is the only thing that keeps the estimation from
    exploding. (The packets should really be unreliable + sequenced.)

--- Sources/Client/NetClient.cpp
+++ Sources/Client/NetClient.cpp
@@ -810,7 +810,7 @@ namespace spades {
 										PosRecord &rec = playerPosRecords[idx];
 										if (rec.valid) {
 											float timespan = GetWorld()->GetTime() - rec.time;
-											timespan = std::max(0.16f, timespan);
+											timespan = std::max(0.08f, timespan);
 											Vector3 vel = (pos - rec.pos) / timespan;
 											vel *= 1.f / 32.f;
 											p->SetVelocity(vel);
