9f1eb8df7e702cbc99a6c81aaf32158745c8cdec
Non <43197300+nonperforming@users.noreply.github.com>
2022-03-23 14:07:42 +1300

    WIP - PubOvl
    Currently non-functional
    
    Add DrDank to AUTHORS... code is based off of theirs

---
+++ Sources/Plus/PubOvl.cpp
@@ -0,0 +1,32 @@
+/*
+
+ Copyright (c) 2022 non_performing
+
+ This file is part of OpenSpades+.
+
+ OpenSpades+ is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ OpenSpades+ is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with OpenSpades+. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "PubOvl.h"
+#include <Core/Settings.h>
+
+DEFINE_SPADES_SETTING(po_names, "1");
+DEFINE_SPADES_SETTING(po_tracers, "0");
+DEFINE_SPADES_SETTING(po_xray, "0");
+
+namespace spades {
+	namespace pubovl {
+	} // namespace client
+} // namespace spades
